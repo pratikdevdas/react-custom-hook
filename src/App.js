6bc1c1428fc2bec83e362d87a65c7ff4c4f8be1c
pratikdevdas <dasdev.pratik@gmail.com>
2022-03-24 16:02:03 +0530

    counter app with custom hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { useState } from 'react';
 import './style.css';
 
+const useCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const increase = () => {
+    setValue(value + 1);
+  };
+
+  const decrease = () => {
+    setValue(value - 1);
+  };
+
+  const zero = () => {
+    setValue(0);
+  };
+
+  return {
+    value,
+    increase,
+    decrease,
+    zero,
+  };
+};
+
 export default function App() {
-  const [counter, setCounter] = useState(0);
+  const counter = useCounter();
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter(0)}>0</button>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>+</button>
+      <button onClick={counter.zero}>0</button>
+      <button onClick={counter.decrease}>-</button>
     </div>
   );
 }
